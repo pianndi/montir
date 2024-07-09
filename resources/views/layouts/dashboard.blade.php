89628adda9cca66f7527b92e1e6338f4c1de1a2b
pianndi <demakalviandi@gmail.com>
2024-07-09 10:48:28 +0700

    host in vercel and https

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Bengkel Kerusakan mobil' }}</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('style.css') }}" />
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/sweetalert2.all.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ secure_asset('style.css') }}" />
+    <script src="{{ secure_asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('js/sweetalert2.all.min.js') }}"></script>
 
 </head>
 
